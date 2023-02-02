0a6c77006947df9356005b91ab7b07222d97adc8
chakrinithesh <chakrinithesh25@gmail.com>
2023-02-02 13:13:43 +0000

    get the vpc

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+import boto3
+import pprint
+from dotenv import load_dotenv
+
+
+def get_aws_keys():
+    load_dotenv()
+    return os.getenv('AKIA4BRNVOTV27G3C4E2'), os.getenv('A6AuuYQ/K1oMreiwqQCOr7IMM0uFINNKKxadfKxn')
+
+def init_aws_session():
+    access_key, secret_key = get_aws_keys()
+    return boto3.Session(aws_access_key_id=access_key, aws_secret_access_key=secret_key, region_name=os.getenv('US-EAST-1'))
+
+def ec2_get_vpc_list():
+    session = init_aws_session()
+    ec2 = session.client('ec2')
+    response = ec2.describe_vpcs()
+    return response['Vpcs']
+
+#python program for vpc details 
