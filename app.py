71d098ff2030331a1a88d9f9d24c1ebc28c77f25
chakrinithesh <chakrinithesh25@gmail.com>
2023-02-03 09:58:30 +0000

    add

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 def get_aws_keys():
     load_dotenv()
-    return os.getenv('XXXX'), os.getenv('XXXXXXX')
+    return os.getenv('XXXX'), os.getenv('XXXXXXXXXXXX')
 
 def init_aws_session():
     access_key, secret_key = get_aws_keys()
