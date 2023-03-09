6bbc2692b8268d8e7ccec32e0ce1ce481ba2e8a0
chakrinithesh <charkinithesh25@gmail.com>
2023-03-09 14:35:32 +0000

    added

--- main.tf
+++ main.tf
@@ -3,6 +3,8 @@ provider "aws" {
     access_key = "${var.aws_access_key}"
     secret_key = "${var.aws_secret_key}"
     region = "us-east-1"
+    endpoints {
+    sts = "https://sts.eu-west-1.amazonaws.com"
 }
 
 terraform {
