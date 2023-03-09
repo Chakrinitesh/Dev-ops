658ee7ca927f942d8fd200afd45d89e2cbc1a72f
Chakrinitesh <101686216+Chakrinitesh@users.noreply.github.com>
2023-03-10 00:14:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,11 +7,7 @@ terraform {
     }
   }
 }
-provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-    region = "${var.aws_region}"
-}
+provider "aws" { }
 
 resource "aws_vpc" "default" {
     cidr_block = "${var.vpc_cidr}"
